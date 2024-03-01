35848498628350ee312a18ba6fa1b5b955007d7c
PANG2OPPA <khbkhb85098509@gmail.com>
2024-03-01 14:58:46 +0900

    2024.03.01 코딩테스트 연습

---
+++ src/알고리즘연습/삼월일일/Ex37.java
@@ -0,0 +1,8 @@
+package 알고리즘연습.삼월일일;
+
+// 정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+public class Ex37 {
+    public int solution(int num, int n) {
+        return (num % n == 0) ? 1 : 0;
+    }
+}
