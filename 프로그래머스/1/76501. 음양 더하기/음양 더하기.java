930f8440bf94d79b70454384719c8b2efc8ad86d
김현빈 <142467047+PANG2OPPA@users.noreply.github.com>
2024-10-17 15:50:07 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int[] absolutes, boolean[] signs) {
+        long answer = 0;
+        for (int i = 0; i < absolutes.length; i++ ){
+                if (signs[i] == true){
+                    answer += absolutes[i];
+                } else {
+                    answer -= absolutes[i];
+                }
+        }
+        return answer;
+    }
+}
